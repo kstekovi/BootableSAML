8d9069d313b026962b4ed412e7973ae380d43b66
Krystof Stekovic <kstekovi@redhat.com>
2023-01-16 09:54:18 +0100

    init

---
+++ src/main/java/org/bootable/jar/saml/MyServlet.java
@@ -0,0 +1,32 @@
+package org.bootable.jar.saml;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.security.Principal;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class MyServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        try (PrintWriter writer = resp.getWriter()) {
+            writer.println("<html>");
+            writer.println("  <head><title>Secured Servlet</title></head>");
+            writer.println("  <body>");
+            writer.println("    <h1>Secured Servlet</h1>");
+            writer.println("    <p>");
+            writer.print(" Current Principal '");
+            Principal user = req.getUserPrincipal();
+            writer.print(user != null ? user.getName() : "NO AUTHENTICATED USER");
+            writer.print("'");
+            writer.println("    </p>");
+            writer.println("  </body>");
+            writer.println("</html>");
+        }
+    }
+
+}
